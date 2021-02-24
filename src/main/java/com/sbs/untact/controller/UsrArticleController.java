30269423dbc4410c25c5677d8ff7cdddc661e375
lucia6758 <lucia6758@gmail.com>
2021-02-25 00:17:49 +0900

    게시물 삭제기능 추가

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -49,4 +49,32 @@ public class UsrArticleController {
 		return rs;
 	}
 
+	@RequestMapping("usr/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		boolean deleteArticleRs = deleteArticle(id);
+
+		Map<String, Object> rs = new HashMap<>();
+		if (deleteArticleRs) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공");
+		} else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당 게시물은 존재하지않습니다.");
+		}
+		rs.put("id", id);
+
+		return rs;
+	}
+
+	private boolean deleteArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
