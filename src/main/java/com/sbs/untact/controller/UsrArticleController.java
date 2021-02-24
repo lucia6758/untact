83f972b9e180f5d11ee0ba77d94c48771b12d853
lucia6758 <lucia6758@gmail.com>
2021-02-25 00:10:56 +0900

    게시물 작성기능 추가

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.sbs.untact.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,10 @@ import com.sbs.untact.dto.Article;
 @Controller
 public class UsrArticleController {
 	private List<Article> articles;
+	private int articlesLastId;
 
 	public UsrArticleController() {
+		articlesLastId = 0;
 		articles = new ArrayList<>();
 
 		articles.add(new Article(1, "2021-02-02", "2021-02-02", "제목1", "내용1"));
@@ -30,6 +34,19 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
+	}
+
+	@RequestMapping("usr/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String updateDate, String title, String body) {
+		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
+
+		Map<String, Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공");
+		rs.put("id", articlesLastId);
+
+		return rs;
 	}
 
 }
