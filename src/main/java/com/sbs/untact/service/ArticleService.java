656976ede0cc3361903c4379ea2101d047480a87
lucia6758 <lucia6758@gmail.com>
2021-03-03 12:40:37 +0900

    마이바티스로 INSERT시 생성된 ID반환

--- src/main/java/com/sbs/untact/service/ArticleService.java
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -1,6 +1,7 @@
 package com.sbs.untact.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.sbs.untact.dao.ArticleDao;
 import com.sbs.untact.dto.Article;
 import com.sbs.untact.dto.ResultData;
+import com.sbs.untact.util.Util;
 
 @Service
 public class ArticleService {
@@ -22,10 +24,10 @@ public class ArticleService {
 		return articleDao.getArticles(searchKeywordType, searchKeyword);
 	}
 
-	public ResultData addArticle(String title, String body) {
-		articleDao.addArticle(title, body);
+	public ResultData addArticle(Map<String, Object> param) {
+		articleDao.addArticle(param);
 
-		int id = 1; // 임시
+		int id = Util.getAsInt(param.get("id"), 0);
 
 		return new ResultData("S-1", "성공", "id", id);
 	}
