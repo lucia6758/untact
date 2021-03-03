a71afadba050c7b813e7049829dd8dde24395d5f
lucia6758 <lucia6758@gmail.com>
2021-03-03 12:44:34 +0900

    마이바티스용 게시물 검색로직 적용

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -45,6 +45,10 @@ public class UsrArticleController {
 			searchKeyword = searchKeyword.trim();
 		}
 
+		if (searchKeyword == null) {
+			searchKeywordType = null;
+		}
+
 		return articleService.getArticles(searchKeywordType, searchKeyword);
 	}
 
