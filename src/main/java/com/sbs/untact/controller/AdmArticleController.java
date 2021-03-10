9c1a911e563a698a2caa7071b05c6c0d161e0b28
lucia6758 <lucia6758@gmail.com>
2021-03-10 18:43:04 +0900

    관리자 리스트 셀렉트박스 버그수정, 관리자게시물작성시 상세페이지로 이동

--- src/main/java/com/sbs/untact/controller/AdmArticleController.java
+++ src/main/java/com/sbs/untact/controller/AdmArticleController.java
@@ -49,6 +49,8 @@ public class AdmArticleController extends BaseConroller {
 
 		Board board = articleService.getBoard(boardId);
 
+		req.setAttribute("board", board);
+
 		if (board == null) {
 			return msgAndBack(req, "존재하지 않는 게시판 입니다.");
 		}
@@ -115,17 +117,16 @@ public class AdmArticleController extends BaseConroller {
 	}
 
 	@RequestMapping("/adm/article/doAdd")
-	@ResponseBody
-	public ResultData doAdd(@RequestParam Map<String, Object> param, HttpServletRequest req,
+	public String doAdd(@RequestParam Map<String, Object> param, HttpServletRequest req,
 			MultipartRequest multipartRequest) {
 		int loginedMemberId = (int) req.getAttribute("loginedMemberId");
 
 		if (param.get("title") == null) {
-			return new ResultData("F-1", "title을 입력해주세요.");
+			return msgAndBack(req, "title을 입력해주세요.");
 		}
 
 		if (param.get("body") == null) {
-			return new ResultData("F-1", "body를 입력해주세요.");
+			return msgAndBack(req, "body를 입력해주세요.");
 		}
 
 		param.put("memberId", loginedMemberId);
@@ -145,7 +146,8 @@ public class AdmArticleController extends BaseConroller {
 
 		}
 
-		return addArticleRd;
+		return msgAndReplace(req, String.format("%d번 게시물이 작성되었습니다.", newArticleId),
+				"../article/detail?id=" + newArticleId);
 	}
 
 	@RequestMapping("/adm/article/doDelete")
