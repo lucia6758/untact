a6dd357c7be760a27577321122c7ec84bf7f50b9
lucia6758 <lucia6758@gmail.com>
2021-03-25 11:42:00 +0900

    게시물삭제, 삭제시 첨부파일도 삭제

--- src/main/java/com/sbs/untact/service/ArticleService.java
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -50,6 +50,8 @@ public class ArticleService {
 	public ResultData deleteArticle(int id) {
 		articleDao.deleteArticle(id);
 
+		genFileService.deleteFiles("article", id);
+
 		return new ResultData("S-1", "글이 삭제되었습니다.", "id", id);
 	}
 
